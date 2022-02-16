92a87c9140a3a74812c43a219e461fb78734b866
Felipe <felipe.araujo@hubfintech.com.br>
2022-02-16 17:50:31 -0300

    chore: :wrench: next translate config

---
+++ i18n.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    "locales": ["pt", "en", "ch"],
+    "defaultLocale": "pt",
+    "pages": {
+        "*": ["common"]
+      },
+      "loadLocaleFrom": (lang, ns) =>
+    // You can use a dynamic import, fetch, whatever. You should
+    // return a Promise with the JSON file.
+    import(`./src/locales/${lang}/${ns}.json`).then((m) => m.default),
+}
--- next.config.js
+++ next.config.js
@@ -1,14 +1,21 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require("next-compose-plugins");
+const nextTranslate = require("next-translate");
 const withPWA = require("next-pwa");
 
-const nextConfig = withPWA({
-  reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
-});
-
+const nextConfig = withPlugins([
+  [
+    withPWA,
+    {
+      reactStrictMode: true,
+      pwa: {
+        dest: "public",
+        register: true,
+        skipWaiting: true,
+        disable: process.env.NODE_ENV === "development",
+      },
+    },
+  ],
+  [nextTranslate],
+]);
 module.exports = nextConfig;
