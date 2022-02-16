4c186b295ce88bda6c1d10693b94f8cad2d59e84
Felipe <felipe.araujo@hubfintech.com.br>
2022-02-16 17:51:15 -0300

    feat: :sparkles: implementing languages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
 import type { NextPage } from "next";
+import { Button, Flex, HStack, Text } from "@chakra-ui/react";
+import useTranslation from "next-translate/useTranslation";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  return <h1>teste</h1>;
+  const { t, lang } = useTranslation("common");
+  return (
+    <Flex direction="column">
+      <HStack>
+        <Link href="/" locale="en" passHref>
+          <Button>EN</Button>
+        </Link>
+        <br />
+
+        <Link href="/" locale="ch" passHref>
+          <Button>ch</Button>
+        </Link>
+        <br />
+
+        <Link href="/" locale="pt" passHref>
+          <Button>pt</Button>
+        </Link>
+        <br />
+      </HStack>
+      <h1>{t("login.helloworld")}</h1>
+      <h1>{lang}</h1>
+
+      <Text>Login</Text>
+    </Flex>
+  );
 };
 
 export default Home;
